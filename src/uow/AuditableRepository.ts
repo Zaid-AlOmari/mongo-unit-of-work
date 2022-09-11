eb6587aca0d4f4b11c4196e4e7ea8572abcedb66
Abdallah Hamdy <abdouelfaresy@yahoo.com>
2022-09-11 12:47:20 +0200

    safe deletion

--- src/uow/AuditableRepository.ts
+++ src/uow/AuditableRepository.ts
@@ -78,7 +78,8 @@ export class AuditableRepository<T extends IAuditable> extends BaseRepository<T>
 
 
   async deleteMany(filter: Filter<T>): Promise<number> {
-    if (!this._configs.softDelete) return super.deleteMany(filter);
+    const newFilter = this.getDeletedFilter(filter);
+    if (!this._configs.softDelete) return super.deleteMany(newFilter);
     const $set = <UpdateFilter<T>['$set']>{ deleted: this.getAuditObject() };
     const result = await this._collection.updateMany(
       filter,
@@ -89,7 +90,8 @@ export class AuditableRepository<T extends IAuditable> extends BaseRepository<T>
   }
 
   async deleteOne(filter: Filter<T>): Promise<T | undefined> {
-    if (!this._configs.softDelete) return super.deleteOne(filter);
+    const newFilter = this.getDeletedFilter(filter);
+    if (!this._configs.softDelete) return super.deleteOne(newFilter);
     const $set = <UpdateFilter<T>['$set']>{ deleted: this.getAuditObject() };
     const result = await this._collection.findOneAndUpdate(
       filter,
